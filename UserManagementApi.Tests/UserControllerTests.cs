5d04210ceca37d3baf408b5570aace7ade493150
ainer <ainer@DESKTOP-HQK72MO>
2024-02-08 09:11:34 +0000

    Added UserManagement unit test project

---
+++ UserManagementApi.Tests/UserControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.IRepositories;
+using DataAccess.Models;
+using Moq;
+using UserManagementApi.Controllers;
+using Xunit;
+
+namespace UserManagementApi.Tests
+{
+    public class UserControllerTests
+    {
+        private Mock<IUserRepository> _repositoryMock;
+
+        public UserControllerTests()
+        {
+            _repositoryMock = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async Task CreateUser_Ok()
+        {
+            var newUser = new User(){
+                FirstName = "Aine",
+                LastName = "Rock",
+                Email = "ainerock@gmail.com"
+            };
+
+            var controller = new UserController(_repositoryMock.Object);
+
+            var result = await controller.CreateUser(newUser);
+            
+            Assert.NotNull(result);
+            _repositoryMock.Verify(repo => repo.AddUserAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
