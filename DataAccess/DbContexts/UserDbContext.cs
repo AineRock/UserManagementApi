8279ea44f2d46e707ee15b7794bf65546d0cf79f
ainer <ainer@DESKTOP-HQK72MO>
2024-02-08 03:02:42 +0000

    Added DAL User model & DbContext class

---
+++ DataAccess/DbContexts/UserDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.DbContexts
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DataAccess/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get;}
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
