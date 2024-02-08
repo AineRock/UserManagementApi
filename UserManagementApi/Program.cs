5815d193d426da7c8a6bf3aab3aa867411103f96
ainer <ainer@DESKTOP-HQK72MO>
2024-02-08 03:16:53 +0000

    Added database connection

--- UserManagementApi/Program.cs
+++ UserManagementApi/Program.cs
@@ -1,8 +1,15 @@
+using DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UserDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("UserManagementConnectionString")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
