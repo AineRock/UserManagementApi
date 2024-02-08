758c3dede88a40f19848e1372f33e7c5bbe91871
ainer <ainer@DESKTOP-HQK72MO>
2024-02-08 01:23:03 +0000

    Added Profile.cs entity class

---
+++ Domain/Entities/Profile.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Profile : BaseEntity
+    {
+        public Profile(string profileName, string description, string email)
+        {
+            ProfileName = profileName;
+            Description = description;
+            Email = email;
+        }
+        public string ProfileName { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+    }
+}
